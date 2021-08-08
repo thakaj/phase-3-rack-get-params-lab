d93a0a486d551754f79d88aa4f7c64453e8bce4e
thakaj <taulant.hakaj@scranton.edu>
2021-08-08 15:17:48 -0400

    commit tree d3838eeafca11da6f5c60ffa0ec238faa57aa98b

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,5 @@
 class Application
-
+  @@cart = []
   @@items = ["Apples","Carrots","Pears"]
 
   def call(env)
@@ -13,6 +13,22 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    end
+      elsif req.path.match(/add/)
+        add_item = req.params['items']
+        if @@items.include? add_item
+          @@cart << add_item
+          resp.write "added #{add_item}"
+        else
+          resp.write "We don't have that item!"
+        end
     else
       resp.write "Path Not Found"
     end
